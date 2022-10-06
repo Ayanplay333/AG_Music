3b73d00077bafeefb7e328737df02f0d918f048e
Ayanplay333 <104997531+Ayanplay333@users.noreply.github.com>
2022-10-06 22:20:38 +0530

    Add files via upload

--- botconfig.js
+++ botconfig.js
@@ -1,28 +1,28 @@
 module.exports = {
   Admins: ["949267155034189844", "949267155034189844"], // Admins of the bot, I don't know what this do. -Darren.
   ExpressServer: true, // If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "a>", // Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "a,", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/Eqan2SGcNz", // Support Server Link
-  Token: process.env.Token || "MTAwNzMwMTQwNDg5OTE1NjA1MA.GTlToT.K7d9S5E_XHmCSBffoAK-2Df6QWABPNsjK3jZlA", // Discord Bot Token
+  SupportServer: "https://discord.gg/VsFFRrCjqr", // Support Server Link
+  Token: process.env.Token || "MTAwNzMwMTQwNDg5OTE1NjA1MA.GEDcGj.rY0NgfRhkTKaMpTQMzJYUruH9Q1FWkNX4qlVLg", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "1007301404899156050", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "L203bbYa9bEn8oeHNXu-h-Cx4TEKmGwL", // Discord Client Secret
+  ClientSecret: process.env.Discord_ClientSecret || "I5qeoAhBMoe9j-KH9glxmYtwd2NJrl-d", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
-  DefaultVolume: 200, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
+  DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
   CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
-  "24/7": true, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
-  CookieSecret: "Pikachu is op", // A cookie for you, cookie for me. make sure you change this value!
+  "24/7": false, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
+  CookieSecret: "Pikachu is so op", // A cookie for you, cookie for me. make sure you change this value!
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
-  EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
+  EmbedColor: "#1364cf", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
   Permissions: 2205281600, // Bot Inviting Permissions
   Website: process.env.Website || "https://DiscoAG-Music.ayanplayz1234.repl.co", // Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
   // If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
 
   Presence: {
     status: "idle", // You can show online, idle, and dnd
-    name: "a>help for music", // The message shown
+    name: "a,help | /help for music", // The message shown
     type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
@@ -30,9 +30,9 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "node1.gglvxd.tk", //- The host name or IP of the lavalink server.
+    host: "lavalink.oops.wtf", //- The host name or IP of the lavalink server.
     port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "free", //- The password of the lavalink server.
+    pass: "www.freelavalink.ga", //- The password of the lavalink server.
     secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
